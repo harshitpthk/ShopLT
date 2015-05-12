0b2c9a275c1f7ce47a1263d999f8f8293e513d7d
harshit <harsht91@gmail.com>
2015-05-12 22:02:56 +0530

    v1.7 final

--- ShopliteCaptureActivity/src/com/homelybuysapp/connection/ConnectionInterface.java
+++
@@ -1,9 +0,0 @@
-package com.homelybuysapp.connection;
-
-
-public interface ConnectionInterface {
-	
-	public void sendRequest( ServiceProvider sp);
-	
-	
-}
--- ShopliteCaptureActivity/src/com/homelybuysapp/interfaces/LocationInterface.java
+++
@@ -1,6 +0,0 @@
-package com.homelybuysapp.interfaces;
-
-public interface LocationInterface {
-
-	public void make_use_of_location();
-}
